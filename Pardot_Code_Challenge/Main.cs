acebf6bf2f3875e517a4ac5e671c8ea74e819282
wgimson <wgimson@gmail.com>
2017-04-25 22:59:10 -0400

    Coding Challenge for Pardot
    Run either by running \bin\Debug\Pardot_Code_Challenge on command line
    and providing argument, or by opening .sln file in Visual Studio and
    running from there

---
+++ Pardot_Code_Challenge/Main.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Pardot_Code_Challenge
+{
+    public class PardotCodeChallenge
+    {
+        static int Main(string[] args)
+        {
+            // make sure an argument is supplied
+            if (args.Length == 0)
+            {
+                Console.WriteLine("--- Please enter a number ---");
+                return 1;
+            }
+
+            int primeLimit;
+            bool isInt = int.TryParse(args[0], out primeLimit);
+            // make sure argument is a number
+            if (isInt == false)
+            {
+                Console.WriteLine("--- Please enter a number ---");
+                return 1;
+            }
+
+            Utilities utils = new Utilities();
+            // get all primes less than argument
+            int[] primes = utils.EratosthenesSieve(primeLimit);
+            // should always be true
+            bool secretAdditiveIsAdditive = utils.SecretIsAdditive(utils.SecretAdditive, primes);
+            Console.WriteLine(secretAdditiveIsAdditive ? "--- Function SecretAdditive is additive, as expected... ---" : "--- Function SecretAdditive is NOT additive, which should never happen... ---");
+            // should always be false
+            bool secretNotAdditiveIsAdditive = utils.SecretIsAdditive(utils.SecretNotAdditive, primes);
+            Console.WriteLine(secretNotAdditiveIsAdditive ? "--- Function SecretNotAdditive is additive, which should never happen... ---" : "--- Function SecretNotAdditive is NOT additive, as expected... ---");
+            // uncomment to print out all primes
+            //Console.WriteLine("-- All Primes Less Than " + primeLimit + " ---");
+            //foreach (int prime in primes)
+            //{
+            //    Console.WriteLine(prime);
+            //}
+            return 0;
+        }
+    }
+}
---
+++ Pardot_Code_Challenge/Utilities.cs
@@ -0,0 +1,84 @@
+﻿
+using System;
+using System.Linq;
+
+namespace Pardot_Code_Challenge
+{
+    public class Utilities
+    {
+        // this function was created to *always* be additive
+        public int SecretAdditive(int number)
+        {
+            return number;
+        }
+        // this function was created to *never* be additive
+        public int SecretNotAdditive(int number)
+        {
+            return (number + 1);
+        }
+
+        // Sieve of Eratosthenes Algorithm; returns every prime number less than 'limit'
+        public int[] EratosthenesSieve(int limit)
+        {
+            int[] numbers = initializeArray(limit);
+            int p = 2;
+            do
+            {
+                for (var i = (2*p); i < limit; i = (i + p))
+                {
+
+                    if (Array.IndexOf(numbers, i) != -1)
+                    {
+                        var pIndex = Array.IndexOf(numbers, i);
+                        // remove multiple of P; cannot be prime
+                        numbers = numbers.Where((val, idx) => idx != pIndex).ToArray();
+                    }
+                }
+
+                p = numbers[Array.IndexOf(numbers, p) + 1];
+            } while (stillHasNumbers(numbers, p));
+
+            // Remove final prime if happens to equal limit (limit is prime)
+            if (numbers[numbers.Length-1] == limit)
+            {
+                numbers = numbers.Where((val, idx) => idx != numbers.Length - 1).ToArray();
+            }
+            return numbers;
+        }
+
+        public bool SecretIsAdditive(Func<int, int> secretFunction, int[] primes)
+        {
+            int xIndex = 0;
+            bool secretIsAdditive = true;
+            for (var yIndex = (xIndex + 1); yIndex < primes.Length; yIndex++)
+            {
+                // First test SecretAdditive
+                // secret(x + y)
+                int xPlusY = primes[xIndex] + primes[yIndex];
+                int secretOfXPlusY = secretFunction(xPlusY);
+                // secret(x) + secret(y)
+                int secretOfX = secretFunction(primes[xIndex]);
+                int secretOfY = secretFunction(primes[yIndex]);
+                int secretOfXPlusSecretOfY = secretOfX + secretOfY;
+                // set to false if secret(x, y) != secret(x) + secret(y)
+                if (secretOfXPlusY != secretOfXPlusSecretOfY)
+                {
+                    secretIsAdditive = false;
+                    // no need to continue
+                    break;
+                }
+            }
+            return secretIsAdditive;
+        }
+
+        private int[] initializeArray(int limit)
+        {
+            return Enumerable.Range(2, (limit - 1)).ToArray();
+        }
+
+        private bool stillHasNumbers(int[] numbers, int p)
+        {
+            return (numbers.ElementAtOrDefault(Array.IndexOf(numbers, p) + 1) != default(int) ? true : false);
+        }
+    }
+}
